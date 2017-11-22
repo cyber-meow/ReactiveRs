2e9ea8d4bab7d590a772b72e533d47ba65b5f956
cyber-meow <sjungle305@gmail.com>
2017-11-22 19:42:44 +0100

    add a test with while and signal

---
+++ src/bin/signal/while_signal.rs
@@ -0,0 +1,50 @@
+extern crate reactive;
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use reactive::{Process, ProcessMut};
+use reactive::process::{value, execute_process};
+use reactive::process::LoopStatus::{Continue, Exit};
+
+use reactive::signal::MpmcSignal;
+
+fn main () {
+    let gather = |x: isize, xs: &mut Vec<isize>| xs.push(x);
+    let mut s = MpmcSignal::new(Vec::new(), gather);
+    let counter = Rc::new(RefCell::new(0));
+    let mut s_clone = s.clone();
+    let counter_clone = counter.clone();
+    let decide_emit = move |n| {
+        let v = *counter_clone.borrow();
+        value(v%2==0)
+        .if_else(s_clone.emit(v), value(()))
+        .map(move |()| n)
+    };
+    let while_p1 = move |n| {
+        if *counter.borrow() == n {
+            Exit(())
+        } else {
+            *counter.borrow_mut() += 1;
+            Continue
+        }
+    };
+    let while_p2 = |v| {
+        println!("Get {:?} at last instant.", v);
+        if v == vec![80] {
+            println!("No more await.");
+            Exit(())
+        } else {
+            Continue
+        }
+    };
+    let p1 = value(100)
+             .and_then(decide_emit)
+             .map(while_p1)
+             .pause()
+             .while_proc();
+    let p2 = s.await()
+             .map(while_p2)
+             .while_proc();
+    execute_process(p1.join(p2));
+}
